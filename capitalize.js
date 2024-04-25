eb7d00b7d50b80ed7d9c002f699871dede4692ef
DavAnders <123442746+DavAnders@users.noreply.github.com>
2024-04-24 22:12:36 -0500

    Modify for tests

--- capitalize.js
+++ capitalize.js
@@ -1,4 +1,7 @@
 function capitalize(string) {
+  if (typeof string !== "string" || string.length === 0) {
+    return "";
+  }
   const newString = string[0].toUpperCase() + string.slice(1);
   return newString;
 }
